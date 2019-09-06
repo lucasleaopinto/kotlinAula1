a7dde8cd55eece2e55a3c40267d1943727b61d2e
lucasleaop <lucasleaop@gmail.com>
2019-09-06 20:19:53 -0300

    Variable e string template

---
+++ app/src/main/java/com/example/myapplication/declaracoes.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+fun main(args: Array<String>) {
+    //Mutável - variavel
+
+    var frase: String = "Hello World"
+
+    println(frase)
+    println("$frase")
+    println("${frase}")
+
+    println("Qtd de chars: ${frase.count()}")
+
+    println("Qtd de chars: ${frase.length}")
+
+    println("Qtd de chars: ${frase.toUpperCase()}")
+
+
+    //Imutável - values
+}
