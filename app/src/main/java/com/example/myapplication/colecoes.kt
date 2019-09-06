879c437755e1699fcf1fccae90b1b19b7d2a4d1d
lucasleaop <lucasleaop@gmail.com>
2019-09-06 20:55:23 -0300

    colecoes

--- app/src/main/java/com/example/myapplication/colecoes.kt
+++ app/src/main/java/com/example/myapplication/colecoes.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     //List(imutável)
-    var familia: List<String> = listOf("Lucas", "Ana","Luana", "Maria", "Tiao")
+    var familia: List<String> = listOf("Lucas", "Ana", "Luana", "Maria", "Tiao")
 
     //percorre com for
-    for (integrantes in familia){
+    for (integrantes in familia) {
         println(integrantes)
     }
+
+
+    //Lista inteiros
+    val listaInteiros: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    for (i in 0..9) {
+        println(listaInteiros[i])
+    }
+
+    val listaInteiros2: List<Int> = (1..10 step 2).toList()
+
+    for (i in listaInteiros2) {
+        println(i)
+    }
+
 }
